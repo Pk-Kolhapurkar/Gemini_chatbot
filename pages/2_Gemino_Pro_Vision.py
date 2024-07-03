ef3fe9ec10ebc593f77f05a029298a0046b60cc4
Prathamesh Khade <121573184+Pk-Kolhapurkar@users.noreply.github.com>
2024-07-04 00:32:31 +0530

    Update 2_Gemino_Pro_Vision.py

--- pages/2_Gemino_Pro_Vision.py
+++ pages/2_Gemino_Pro_Vision.py
@@ -3,29 +3,44 @@ import google.generativeai as genai
 import streamlit as st
 import time
 import random
-from utils import SAFETY_SETTTINGS
+from utils import SAFETY_SETTINGS
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Sets the avatar for user as well as the bot
+image_path = "Google-Gemini-AI-Logo.png"
 
 st.set_page_config(
-    page_title="Chat To XYthing",
-    page_icon="🔥",
+    page_title="Chat with image",
+    page_icon="🗣️",
     menu_items={
-        'About': "# Make by hiliuxg"
+        'About': "# Made by Prathamesh Khade"
     }
 )
 
-st.title('Upload Image And Ask')
+# Sidebar for the Gemini logo and clear chat button
+st.sidebar.image(image_path, caption='Gemini AI', use_column_width=True)
+st.sidebar.title("Options")
+if st.sidebar.button("Clear Chat Window", use_container_width=True, type="primary"):
+    clear_state()  # Call the function to clear the chat window
+
+st.title('Upload Image And Chat with Image')
 
 if "app_key" not in st.session_state:
-    app_key = st.text_input("Your Gemini App Key", type='password')
+    st.markdown(
+        "To use this app, you need a Gemini API key. If you don't have one, you can create it "
+        "[here](https://aistudio.google.com/app/apikey)."
+    )
+    app_key = st.text_input("Enter your Gemini App Key below", type='password', key='api_key_input')
     if app_key:
         st.session_state.app_key = app_key
 
 try:
-    genai.configure(api_key = st.session_state.app_key)
+    genai.configure(api_key=st.session_state.app_key)
     model = genai.GenerativeModel('gemini-pro-vision')
 except AttributeError as e:
     st.warning("Please Put Your Gemini App Key First.")
-
 
 def show_message(prompt, image, loading_str):
     with st.chat_message("assistant"):
@@ -33,7 +48,7 @@ def show_message(prompt, image, loading_str):
         message_placeholder.markdown(loading_str)
         full_response = ""
         try:
-            for chunk in model.generate_content([prompt, image], stream = True, safety_settings = SAFETY_SETTTINGS):                   
+            for chunk in model.generate_content([prompt, image], stream=True, safety_settings=SAFETY_SETTINGS):
                 word_count = 0
                 random_int = random.randint(5, 10)
                 for word in chunk.text:
@@ -54,19 +69,17 @@ def show_message(prompt, image, loading_str):
 def clear_state():
     st.session_state.history_pic = []
 
-
 if "history_pic" not in st.session_state:
     st.session_state.history_pic = []
 
-
 image = None
 if "app_key" in st.session_state:
-    uploaded_file = st.file_uploader("choose a pic...", type=["jpg", "png", "jpeg", "gif"], label_visibility='collapsed', on_change = clear_state)
+    uploaded_file = st.file_uploader("Choose a pic...", type=["jpg", "png", "jpeg", "gif"], label_visibility='collapsed', on_change=clear_state)
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         width, height = image.size
-        resized_img = image.resize((128, int(height/(width/128))), Image.LANCZOS)
-        st.image(image)    
+        resized_img = image.resize((128, int(height / (width / 128))), Image.LANCZOS)
+        st.image(image)
 
 if len(st.session_state.history_pic) > 0:
     for item in st.session_state.history_pic:
@@ -74,7 +87,7 @@ if len(st.session_state.history_pic) > 0:
             st.markdown(item["text"])
 
 if "app_key" in st.session_state:
-    if prompt := st.chat_input("desc this picture"):
+    if prompt := st.chat_input("Describe this picture"):
         if image is None:
             st.warning("Please upload an image first", icon="⚠️")
         else:
@@ -82,5 +95,6 @@ if "app_key" in st.session_state:
             with st.chat_message("user"):
                 st.markdown(prompt)
                 st.session_state.history_pic.append({"role": "user", "text": prompt})
-            
+
             show_message(prompt, resized_img, "Thinking...")
+            
