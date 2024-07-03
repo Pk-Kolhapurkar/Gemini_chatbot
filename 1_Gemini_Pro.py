8f10b6683281403c9c0fff8e75e9424eb6a93742
Prathamesh Khade <121573184+Pk-Kolhapurkar@users.noreply.github.com>
2024-07-04 00:06:12 +0530

    Update 1_Gemini_Pro.py

--- 1_Gemini_Pro.py
+++ 1_Gemini_Pro.py
@@ -1,11 +1,10 @@
-
-
+from PIL import Image
 import google.generativeai as genai
 import streamlit as st
 import time
 import random
-from utils import SAFETY_SETTTINGS
 from dotenv import load_dotenv
+from utils import SAFETY_SETTTINGS
 
 load_dotenv()
 
@@ -26,7 +25,6 @@ if st.sidebar.button("Clear Chat Window", use_container_width=True, type="primar
 
 st.title('Upload Image and Chat with Image')
 
-
 # API Key input section
 if "app_key" not in st.session_state:
     st.markdown(
@@ -41,25 +39,20 @@ if "history" not in st.session_state:
     st.session_state.history = []
 
 try:
-    genai.configure(api_key = st.session_state.app_key)
+    genai.configure(api_key=st.session_state.app_key)
 except AttributeError as e:
     st.warning("Please Put Your Gemini App Key First.")
 
 model = genai.GenerativeModel('gemini-pro')
-chat = model.start_chat(history = st.session_state.history)
+chat = model.start_chat(history=st.session_state.history)
 
-with st.sidebar:
-    if st.button("Clear Chat Window", use_container_width = True, type="primary"):
-        st.session_state.history = []
-        st.rerun()
-    
 for message in chat.history:
     role = "assistant" if message.role == "model" else message.role
     with st.chat_message(role):
         st.markdown(message.parts[0].text)
 
 if "app_key" in st.session_state:
-    if prompt := st.chat_input(""):
+    if prompt := st.chat_input("Describe this picture"):
         prompt = prompt.replace('\n', '  \n')
         with st.chat_message("user"):
             st.markdown(prompt)
@@ -69,7 +62,7 @@ if "app_key" in st.session_state:
             message_placeholder.markdown("Thinking...")
             try:
                 full_response = ""
-                for chunk in chat.send_message(prompt, stream=True, safety_settings = SAFETY_SETTTINGS):
+                for chunk in chat.send_message(prompt, stream=True, safety_settings=SAFETY_SETTTINGS):
                     word_count = 0
                     random_int = random.randint(5, 10)
                     for word in chunk.text:
